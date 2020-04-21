e189b46cfdfadff0f75c0479b47e3ff251f7a37b
Daniel Jones <daniel.jones@engineerbetter.com>
2020-04-21 15:54:41 +0100

    zero-downtime deploy used app name that nothing else did

--- resource/commands/cancel-zero-downtime-push.sh
+++ resource/commands/cancel-zero-downtime-push.sh
@@ -1,8 +1,8 @@
-current_app_name=$(get_option '.current_app_name')
+app_name=$(get_option '.app_name')
 
-logger::info "Executing $(logger::highlight "$command"): $current_app_name"
+logger::info "Executing $(logger::highlight "$command"): $app_name"
 
 cf::target "$org" "$space"
 
 # autopilot (tested v0.0.2 - v0.0.6) doesn't like CF_TRACE=true
-CF_TRACE=false cf::cf v3-cancel-zdt-push "$current_app_name"
+CF_TRACE=false cf::cf v3-cancel-zdt-push "$app_name"
