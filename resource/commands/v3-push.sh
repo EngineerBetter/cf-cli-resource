607c93b082d67566f040acdf48d3bc765cf8692f
Daniel Jones <daniel.jones@engineerbetter.com>
2020-04-21 16:19:08 +0100

    get v3-push to work with new function names

--- resource/commands/v3-push.sh
+++ resource/commands/v3-push.sh
@@ -1,49 +1,54 @@
-    app_name=$(echo $options | jq -r '.app_name //empty')
-    buildpack=$(echo $options | jq -r '.buildpack //empty')
-    startup_command=$(echo $options | jq -r '.startup_command //empty')
-    docker_image=$(echo $options | jq -r '.docker_image //empty')
-    docker_username=$(echo $options | jq -r '.docker_username //empty')
-    docker_password=$(echo $options | jq -r '.docker_password //empty')
-    manifest=$(echo $options | jq -r '.manifest //empty')
-    instances=$(echo $options | jq -r '.instances //empty')
-    disk_quota=$(echo $options | jq -r '.disk_quota //empty')
-    memory=$(echo $options | jq -r '.memory //empty')
-    no_start=$(echo $options | jq -r '.no_start //empty')
-    path=$(echo $options | jq -r '.path //empty')
-    stack=$(echo $options | jq -r '.stack //empty')
-    vars=$(echo $options | jq -r '.vars //empty')
-    vars_files=$(echo $options | jq -r '.vars_files //empty')
-    staging_timeout=$(echo $options | jq -r '.staging_timeout //0')
-    startup_timeout=$(echo $options | jq -r '.startup_timeout //0')
+app_name=$(get_option '.app_name')
+buildpack=$(get_option '.buildpack')
+startup_command=$(get_option '.startup_command')
+docker_image=$(get_option '.docker_image')
+docker_username=$(get_option '.docker_username')
+docker_password=$(get_option '.docker_password')
+manifest=$(get_option '.manifest')
+instances=$(get_option '.instances')
+disk_quota=$(get_option '.disk_quota')
+memory=$(get_option '.memory')
+no_start=$(get_option '.no_start')
+path=$(get_option '.path')
+stack=$(get_option '.stack')
+vars=$(get_option '.vars')
+vars_files=$(get_option '.vars_files')
+staging_timeout=$(get_option '.staging_timeout' 0)
+startup_timeout=$(get_option '.startup_timeout' 0)
 
-    args=()
-    [ -n "$app_name" ]        && args+=("$app_name")
-    [ -n "$buildpack" ]       && args+=(-b "$buildpack")
-    [ -n "$startup_command" ] && args+=(-c "$startup_command")
-    [ -n "$docker_image" ]    && args+=(--docker-image "$docker_image")
-    [ -n "$docker_username" ] && args+=(--docker-username "$docker_username")
-    [ -n "$docker_password" ] && export CF_DOCKER_PASSWORD="$docker_password"
-    [ -n "$manifest" ]        && args+=(-f "$manifest")
-    [ -n "$instances" ]       && args+=(-i "$instances")
-    [ -n "$disk_quota" ]      && args+=(-k "$disk_quota")
-    [ -n "$memory" ]          && args+=(-m "$memory")
-    [ -n "$no_start" ]        && args+=(--no-start)
-    [ -n "$path" ]            && args+=(-p $path) # don't quote so we can support globbing
-    [ -n "$stack" ]           && args+=(-s "$stack")
+if [ ! -f "$manifest" ]; then
+  logger::error "invalid payload (manifest is not a file: $manifest)"
+  exit $E_MANIFEST_FILE_NOT_FOUND
+fi
 
-    for key in $(echo $vars | jq -r 'keys[]'); do
-      value=$(echo $vars | jq -r --arg key "$key" '.[$key]')
-      args+=(--var "$key=$value")
-    done
+args=()
+[ -n "$app_name" ]        && args+=("$app_name")
+[ -n "$buildpack" ]       && args+=(-b "$buildpack")
+[ -n "$startup_command" ] && args+=(-c "$startup_command")
+[ -n "$docker_image" ]    && args+=(--docker-image "$docker_image")
+[ -n "$docker_username" ] && args+=(--docker-username "$docker_username")
+[ -n "$docker_password" ] && export CF_DOCKER_PASSWORD="$docker_password"
+[ -n "$manifest" ]        && args+=(-f "$manifest")
+[ -n "$instances" ]       && args+=(-i "$instances")
+[ -n "$disk_quota" ]      && args+=(-k "$disk_quota")
+[ -n "$memory" ]          && args+=(-m "$memory")
+[ -n "$no_start" ]        && args+=(--no-start)
+[ -n "$path" ]            && args+=(-p $path) # don't quote so we can support globbing
+[ -n "$stack" ]           && args+=(-s "$stack")
 
-    for vars_file in $(echo $vars_files | jq -r '.[]'); do
-      args+=(--vars-file "$vars_file")
-    done
+for key in $(echo $vars | jq -r 'keys[]'); do
+  value=$(echo $vars | jq -r --arg key "$key" '.[$key]')
+  args+=(--var "$key=$value")
+done
 
-    logger::info "Executing $(logger::highlight "$command"): $app_name"
-    cf_target "$org" "$space"
+for vars_file in $(echo $vars_files | jq -r '.[]'); do
+  args+=(--vars-file "$vars_file")
+done
 
-    [ "$staging_timeout" -gt "0" ] && export CF_STAGING_TIMEOUT=$staging_timeout
-    [ "$startup_timeout" -gt "0" ] && export CF_STARTUP_TIMEOUT=$startup_timeout
+logger::info "Executing $(logger::highlight "$command"): $app_name"
+cf::target "$org" "$space"
 
-    cf7 push --strategy rolling "${args[@]}"
+[ "$staging_timeout" -gt "0" ] && export CF_STAGING_TIMEOUT=$staging_timeout
+[ "$startup_timeout" -gt "0" ] && export CF_STARTUP_TIMEOUT=$startup_timeout
+
+cf7 push --strategy rolling "${args[@]}"
